40767ae88180e973d7277c333a895e9765d7d6b9
Pablo Aldana <thorinroble@gmail.com>
2024-02-24 17:00:16 +0100

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,29 @@
+## 0.3.0
+
+* Added page navigation observer support
+
+## 0.2.1
+
+* Updated Readme, example and version bump
+
+## 0.1.4
+
+* Enabled for flutter web
+* Added plausible event props
+* Added example
+
+## 0.1.3
+
+* Added web check
+
+## 0.1.2
+
+* Added error handling for http request.
+
+## 0.1.1
+
+* Added disable feature.
+
+## 0.1.0
+
+* Initial working release.
---
+++ test/plausible_analytics_test.dart
@@ -0,0 +1,56 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:plausible_analytics/plausible_analytics.dart';
+
+const String serverUrl = "https://analytics.bostrot.com";
+const String domain = "example.com";
+const String name = "pageview";
+const String url = "app://localhost/test";
+const String referrer = "app://localhost/referrer";
+const String screenWidth = "1000x1000";
+
+void main() {
+  test('make plausible event call with pageview', () async {
+    final plausible = Plausible(serverUrl, domain, screenWidth: screenWidth);
+    expect(plausible.serverUrl, serverUrl);
+    expect(plausible.domain, domain);
+    expect(plausible.screenWidth, screenWidth);
+
+    final event = plausible.event();
+    expect(await event, 202);
+  });
+
+  test('make plausible event call with custom event', () async {
+    final plausible = Plausible(serverUrl, domain, screenWidth: screenWidth);
+    expect(plausible.serverUrl, serverUrl);
+    expect(plausible.domain, domain);
+    expect(plausible.screenWidth, screenWidth);
+
+    final event = plausible.event(
+        name: 'conversion', page: 'homescreen', referrer: 'referrerPage');
+    expect(await event, 202);
+  });
+
+  test('check disabled call', () async {
+    final plausible = Plausible(serverUrl, domain, screenWidth: screenWidth);
+    plausible.enabled = false;
+    expect(plausible.serverUrl, serverUrl);
+    expect(plausible.domain, domain);
+    expect(plausible.screenWidth, screenWidth);
+
+    final event = plausible.event(
+        name: 'conversion', page: 'homescreen', referrer: 'referrerPage');
+    expect(await event, 0);
+  });
+
+  test('check failed http request', () async {
+    final plausible =
+        Plausible("somewrongurl.asd21", domain, screenWidth: screenWidth);
+    expect(plausible.serverUrl, "somewrongurl.asd21");
+    expect(plausible.domain, domain);
+    expect(plausible.screenWidth, screenWidth);
+
+    final event = plausible.event(
+        name: 'conversion', page: 'homescreen', referrer: 'referrerPage');
+    expect(await event, 1);
+  });
+}
